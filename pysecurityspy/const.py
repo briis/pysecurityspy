4ec3f2a3d86f1c253c0f9c93c819121ffeacb4f7
“Bjarne <bjarne@briis.com>
2020-06-30 09:24:07 +0200

    Initial setup

---
+++ pysecurityspy/const.py
@@ -0,0 +1,13 @@
+"""Constant definitions for pysecurityspy."""
+
+TRIGGER_TYPE = {
+    1: "Video motion detection",
+    2: "Audio detection",
+    4: "AppleScript",
+    8: "Camera event",
+    16: "Web server event",
+    32: "Triggered by another camera",
+    64: "Manual trigger",
+    128: "Human",
+    256: "Vehicle",
+}
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'pysecurityspy',
+  packages=['pysecurityspy'],
+  version = '0.1.0',
+  license='MIT',
+  description = 'Python Wrapper for SecuritySpy API', 
+  author = 'Bjarne Riis',
+  author_email = 'bjarne@briis.com',
+  url = 'https://github.com/briis/pysecurityspy',
+  keywords = ['SecuritySpy', 'Surveillance', 'Python', 'Home-Assistant'],
+  install_requires=[
+          'asyncio',
+          'aiohttp',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
