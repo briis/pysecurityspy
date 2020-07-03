55f3e5c42ca641070a92978287c5a0617fc63f9b
“Bjarne <bjarne@briis.com>
2020-07-03 14:01:08 +0200

    v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pysecurityspy',
   packages=['pysecurityspy'],
-  version = '0.1.1',
+  version = '0.1.2',
   license='MIT',
   description = 'Python Wrapper for SecuritySpy API', 
   author = 'Bjarne Riis',
